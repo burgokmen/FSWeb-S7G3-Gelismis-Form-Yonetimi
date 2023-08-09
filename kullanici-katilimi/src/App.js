c4f70646825d019e04037f15deabf2d70540f2b9
burgokmen <burgokmen@gmail.com>
2023-08-09 18:58:07 +0300

    init

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormMaker from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Form Example</p>
+        <div>
+          <h1>Form Example</h1>
+        </div>
+        <div>
+          <FormMaker />
+        </div>
       </header>
     </div>
   );
---
+++ kullanici-katilimi/src/components/Form.js
@@ -0,0 +1,11 @@
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+const FormMaker = () => {
+  return;
+
+  <Form>
+    <h1>Form here</h1>
+  </Form>;
+};
+
+export default FormMaker;
